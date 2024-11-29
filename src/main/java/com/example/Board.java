c7169ba38a91b15e9b6ed4336af8f4c9a0b9bbcf
szilagyidaniel0327@gmail.com <63243766+mortagehu@users.noreply.github.com>
2024-11-29 19:02:55 +0100

    Board and Player
    Board still needs some touches but player looks good

--- src/main/java/com/example/Board.java
+++ src/main/java/com/example/Board.java
@@ -1,4 +1,66 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
+    private final int rows;
+    private final int columns;
+    private final String[][] grid;
+
+    public Board(int rows, int columns){
+        this.rows = rows;
+        this.columns = columns;
+        this.grid = new String[rows][columns];
+    }
+
+
+    public void displayBoard(){
+        for (int i = rows -1; i >= 0; i--){
+            for(int j = 0; j < columns; j++){
+                System.out.println(grid[i][j] == null ? ".": grid[i][j].charAt(0));
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    //TODO columnToIndex-et implementálni
+
+
+    public boolean placeDisk(String column, String color){
+        int colIndex = columnToIndex(column);
+        if (colIndex == -1 || colIndex >= columns){
+            return false; // érvéntelen oszlop
+        }
+
+        for (int i = 0; i < rows; i++){
+            if (grid[i][colIndex] == null){
+                grid[i][colIndex] = color;
+                return true; //Ha sikerült elhelyezni egy disket
+            }
+        }
+        return false; //Ha tele van a column
+    }
+
+    public List<String> getValidColumns(){
+        List<String> validColumns = new ArrayList<>();
+        for (int col = 0; col < columns; col++){
+            if (grid[rows - 1][col] == null){
+                validColumns.add(indexToColumn(col));
+            }
+        }
+        return validColumns;
+    }
+
+    private int columnToIndex(String column){
+        return column.toLowerCase().charAt(0)- 'a';
+    }
+    private String indexToColumn(int index) {
+        return String.valueOf((char) ('a' + index));
+    }
+    public boolean checkWinner(String color){
+        return checkVertical(color) || checkHorizontal(color) || checkDiagonal(color);
+    }
 }
--- src/main/java/com/example/Player.java
+++ src/main/java/com/example/Player.java
@@ -1,4 +1,20 @@
 package com.example;
 
-public class Player {
+public abstract class Player {
+    private final String name;
+    private final String color;
+
+    public Player(String name, String color){
+        this.name = name;
+        this.color = color;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public String getColor(){
+        return color;
+    }
+
+    public abstract String makeMove();
 }
